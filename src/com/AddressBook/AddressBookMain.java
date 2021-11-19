6d8e5a8d9de32d4ea272b418596f1c55e2e0e220
memories2013 <nshivaprasad41@gmail.com>
2021-11-19 15:08:15 +0530

    Ability to add new Contact added

--- src/com/AddressBook/AddressBookMain.java
+++ src/com/AddressBook/AddressBookMain.java
@@ -3,7 +3,10 @@ package com.AddressBook;
 public class AddressBookMain {
 	
 	public static void main(String[] args) {
-			System.out.println("Welcome to Address Book Program");
-
+		System.out.println("--------------------Welcome To Address Book Program-----------------------");
+		Contact_Methods obj = new Contact_Methods();
+		obj.addContact();
+		obj.displayContacts();
+		
 	}
 }
